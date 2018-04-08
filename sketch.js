4a0faf305277cb9c8c337b48f7287437325ac0a0
Nick Chen <nickchen@Nicks-Air.home>
2018-04-08 00:12:44 -0400

    reverse storage array. So #app will show the latest searched word at 1st

--- sketch.js
+++ sketch.js
@@ -16,22 +16,21 @@ rate.onchange = function(){
   chrome.storage.sync.set({'value': rate.value}, ()=>console.log('Setting saved'));
 }
 
-//Dynamic innerHTML
+//Dynamic innerHTML for #app
 chrome.storage.sync.get('allWords',(data)=>{  
 
   console.log(data.allWords);
 
-  var slide = data.allWords.forEach((el)=>{
+  var slide = data.allWords.reverse().forEach((el)=>{
 
-      document.getElementById('app').innerHTML += '<div class="slide">'+el[0]+'</div>';
-  
+      document.getElementById('app').innerHTML += '<div class="slide" style="cursor:pointer;width:200px;">'+el[0]+'</div>';
+
   });
 
-})
-
 //slideshow built here
+  var n = data.allWords.length-1;
   document.getElementById('left').addEventListener('click',()=>{
-    if(distance>-800 && distance<=0){
+    if(distance>-(n*200) && distance<=0){
       distance-=200;
       document.getElementById('app').style.transform = `translateX(${distance}px)`;
     }else{
@@ -41,12 +40,39 @@ chrome.storage.sync.get('allWords',(data)=>{
   });
 
   document.getElementById('right').addEventListener('click',()=>{
-    if(distance>=-800 && distance<0){
+    if(distance>=-(n*200) && distance<0){
       distance+=200;
       document.getElementById('app').style.transform = `translateX(${distance}px)`;
     }else{
-      distance-=800;
+      distance-=(n*200);
       document.getElementById('app').style.transform = `translateX(${distance}px)`;
     }
   });
+
+
+})
+
+
+
+
+// //slideshow built here
+//   document.getElementById('left').addEventListener('click',()=>{
+//     if(distance>-800 && distance<=0){
+//       distance-=200;
+//       document.getElementById('app').style.transform = `translateX(${distance}px)`;
+//     }else{
+//       distance=0;
+//       document.getElementById('app').style.transform = `translateX(${distance}px)`;
+//     }
+//   });
+
+//   document.getElementById('right').addEventListener('click',()=>{
+//     if(distance>=-800 && distance<0){
+//       distance+=200;
+//       document.getElementById('app').style.transform = `translateX(${distance}px)`;
+//     }else{
+//       distance-=800;
+//       document.getElementById('app').style.transform = `translateX(${distance}px)`;
+//     }
+//   });
 
