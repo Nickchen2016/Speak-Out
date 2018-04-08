53d7186cda4b061fb56bf7c03259133f3009da38
Nick Chen <nickchen@Nicks-Air.home>
2018-04-08 17:35:49 -0400

    popu-up page words definition showing

--- sketch.js
+++ sketch.js
@@ -16,22 +16,36 @@ rate.onchange = function(){
   chrome.storage.sync.set({'value': rate.value}, ()=>console.log('Setting saved'));
 }
 
-//Dynamic innerHTML for #app
+
 chrome.storage.sync.get('allWords',(data)=>{  
 
   console.log(data.allWords);
 
-  var slide = data.allWords.reverse().forEach((el)=>{
-
-      document.getElementById('app').innerHTML += '<div class="slide" style="cursor:pointer;width:200px;">'+el[0]+'</div>';
+  data.allWords.reverse().forEach((el)=>{
+//Dynamic innerHTML for #app
+      document.getElementById('app').innerHTML += '<div class="slide" style="cursor:pointer;width:315px;height:100%;">'+el[0][0].toUpperCase()+el[0].slice(1)+'</div>';
 
   });
+
+  //Dynamic click event for #definition
+  var className = document.getElementsByClassName('slide');
+  // console.log('----------', className[0].innerHTML);
+  Array.from(className).forEach((element)=>{
+    element.addEventListener('click',()=>{
+      // console.log('element here', element.textContent);
+     data.allWords.forEach((el)=>{
+      if(element.textContent.toLowerCase()===el[0]){
+        document.getElementById('definition').innerHTML = '<div style="display:flex;"><div style="background:red;width:2%;height:36px;margin-top:35px;"></div><div style="width:95%;height:30px;font-family:futura_bold;font-size:33px;margin-top:36px;margin-left:20px;">'+el[0][0].toUpperCase()+el[0].slice(1)+' :</div></div><div style="width:85%;height:30px;margin-top:10px;margin-left:28px;margin-bottom:42px;">'+el[1]+'</div>';
+        }
+     })
+    })
+  })
 
 //slideshow built here
   var n = data.allWords.length-1;
   document.getElementById('left').addEventListener('click',()=>{
-    if(distance>-(n*200) && distance<=0){
-      distance-=200;
+    if(distance>-(n*315) && distance<=0){
+      distance-=315;
       document.getElementById('app').style.transform = `translateX(${distance}px)`;
     }else{
       distance=0;
@@ -40,11 +54,11 @@ chrome.storage.sync.get('allWords',(data)=>{
   });
 
   document.getElementById('right').addEventListener('click',()=>{
-    if(distance>=-(n*200) && distance<0){
-      distance+=200;
+    if(distance>=-(n*315) && distance<0){
+      distance+=315;
       document.getElementById('app').style.transform = `translateX(${distance}px)`;
     }else{
-      distance-=(n*200);
+      distance-=(n*315);
       document.getElementById('app').style.transform = `translateX(${distance}px)`;
     }
   });
@@ -52,27 +66,8 @@ chrome.storage.sync.get('allWords',(data)=>{
 
 })
 
+document.getElementById('triangle').addEventListener('click', ()=>{
+  document.getElementById('definition').innerHTML = '';
+})
 
-
-
-// //slideshow built here
-//   document.getElementById('left').addEventListener('click',()=>{
-//     if(distance>-800 && distance<=0){
-//       distance-=200;
-//       document.getElementById('app').style.transform = `translateX(${distance}px)`;
-//     }else{
-//       distance=0;
-//       document.getElementById('app').style.transform = `translateX(${distance}px)`;
-//     }
-//   });
-
-//   document.getElementById('right').addEventListener('click',()=>{
-//     if(distance>=-800 && distance<0){
-//       distance+=200;
-//       document.getElementById('app').style.transform = `translateX(${distance}px)`;
-//     }else{
-//       distance-=800;
-//       document.getElementById('app').style.transform = `translateX(${distance}px)`;
-//     }
-//   });
 
